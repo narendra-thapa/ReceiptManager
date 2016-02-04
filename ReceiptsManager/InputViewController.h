a788960d77d52a01a31d240f56619f81d0d00a3d
narendrathapa <thapa.narendra@gmail.com>
2016-02-04 12:25:15 -0500

    Initial project setup - W4D4

---
+++ ReceiptsManager/InputViewController.h
@@ -0,0 +1,13 @@
+//
+//  InputViewController.h
+//  ReceiptsManager
+//
+//  Created by Narendra Thapa on 2016-02-04.
+//  Copyright © 2016 Narendra Thapa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InputViewController : UIInputViewController
+
+@end
