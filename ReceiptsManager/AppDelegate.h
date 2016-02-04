8348a15fb6e9d2d5371d41e4857200c96bd0dce2
narendrathapa <thapa.narendra@gmail.com>
2016-02-04 12:01:50 -0500

    Initial Commit

---
+++ ReceiptsManager/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  ReceiptsManager
+//
+//  Created by Narendra Thapa on 2016-02-04.
+//  Copyright © 2016 Narendra Thapa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ ReceiptsManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReceiptsManager
+//
+//  Created by Narendra Thapa on 2016-02-04.
+//  Copyright © 2016 Narendra Thapa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
